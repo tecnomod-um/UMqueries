689a7faf00219763bf2ae3c58413df316fbb8022
danibanezRepos <danibanez.info@gmail.com>
2023-04-04 11:53:45 +0200

    Establishing main project structure

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,2 +1,15 @@
+import React from "react";
 import MainStyles from "./Main.module.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
+// Main view
+function Main() {
+    return (
+      <span>
+        <h1>UMU - BIOQUERIES</h1>
+        <div className={MainStyles.container}>
+        </div>
+      </span>
+    );
+  }
+  export default Main;
