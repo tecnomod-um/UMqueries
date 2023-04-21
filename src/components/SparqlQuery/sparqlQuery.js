8049d98ea394f7a75f9be656f625e9e7796385e8
danibanezRepos <danibanez.info@gmail.com>
2023-04-21 15:18:56 +0200

    sparqlQuery component needs some work

--- src/components/SparqlQuery/sparqlQuery.js
+++ src/components/SparqlQuery/sparqlQuery.js
@@ -8,7 +8,7 @@ const SparqlQuery = ({ url, sparqlQuery, queryResult, setQueryResult }) => {
     const handleQuery = () => {
         setIsLoading(true);
         axios
-            .get(url)
+            .get('/api/http://ssb4.nt.ntnu.no:10022/sparql?default-graph-uri=&query=SELECT+%3Fs+%3Fp+%3Fo%0D%0AFROM+%3Chttp%3A%2F%2Frdf.biogateway.eu%2Fgraph%2Fgene%3E%0D%0AWHERE+%7B%0D%0A++%3Fs+%3Fp+%3Fo+.%0D%0A%7D+limit+50&format=application%2Fsparql-results%2Bjson&timeout=0&signal_void=on')
             .then((res) => {
                 setQueryResult(res.data);
             });
