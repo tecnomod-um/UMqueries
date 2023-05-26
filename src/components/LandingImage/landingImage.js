1d580fabf311f9efbab17d8d28f6b56d07dd3fb8
danibanez <danibanez.info@gmail.com>
2023-05-26 15:10:02 +0200

    Started work on Landing page

---
+++ src/components/LandingImage/landingImage.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef, useState } from "react";
+import LandingImageStyles from "./landingImage.module.css";
+
+const LandingImage = ({ imageSrc, width, height }) => {
+    const [animationOffset, setAnimationOffset] = useState(1);
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const imageElement = imageRef.current;
+            if (imageElement) {
+                const { innerHeight } = window;
+                const { top, height } = imageElement.getBoundingClientRect();
+                const offset = innerHeight - top;
+
+                if (offset > 0 && offset < innerHeight + height) {
+                    setAnimationOffset(Math.min(offset / (innerHeight / 3), 1));
+                }
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className={LandingImageStyles.image} style={{ opacity: animationOffset }}>
+            <img ref={imageRef} src={imageSrc} alt="Sample" width={width} height={height} />
+        </div>
+    );
+};
+
+export default React.memo(LandingImage);
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,19 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import MainStyles from "./main.module.css";
+import LandingImage from "../components/LandingImage/landingImage";
 
-// Landing page. Brief instructions on how to use the tool will be shown here.
+import image from "../resources/images/image.jpg";
+
+// Brief introduction to the tool
 function Main() {
+  const [scrollOffset, setScrollOffset] = useState(0);
+  const [showScrollIndicator, setShowScrollIndicator] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollOffset(window.pageYOffset);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScrollIndicator = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+
+      setShowScrollIndicator(scrollOffset < documentHeight - windowHeight);
+    };
+
+    window.addEventListener("scroll", handleScrollIndicator);
+    handleScrollIndicator();
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollIndicator);
+    };
+  }, [scrollOffset]);
+
+  const handleScrollToBottom = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <span>
-      <h1 className={MainStyles.mainHeader}>UM queries</h1>
+    <div className={MainStyles.pageContainer}>
       <div className={MainStyles.mainContainer}>
-        <div className={MainStyles.text}>(This is where the landing page will go)</div>
-        <Link to={'/queries'}>
-          <button className={MainStyles.big_button}>Get started</button>
-        </Link>
+        <h1 className={MainStyles.mainHeader}>UM queries</h1>
+        <div className={MainStyles.contentContainer}>
+          <div className={MainStyles.textImageContainer}>
+            <div className={MainStyles.textContainer}>
+              <p className={MainStyles.introText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac justo rhoncus, aliquam magna eu,
+                pulvinar nisi. Suspendisse sit amet sagittis dolor, sed fringilla dui. In sit amet eros et ligula
+                placerat vestibulum. Curabitur lobortis consectetur ipsum, eget tincidunt tellus ultricies a.
+              </p>
+            </div>
+            <div className={MainStyles.imageWrapper}>
+              <LandingImage imageSrc={image} width={500} height={350} />
+            </div>
+          </div>
+          <div className={MainStyles.textImageContainer}>
+            <div className={MainStyles.imageWrapper}>
+              <LandingImage imageSrc={image} width={500} height={350} />
+            </div>
+            <div className={MainStyles.textContainer}>
+              <p className={MainStyles.introText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac justo rhoncus, aliquam magna eu,
+                pulvinar nisi. Suspendisse sit amet sagittis dolor, sed fringilla dui. In sit amet eros et ligula
+                placerat vestibulum. Curabitur lobortis consectetur ipsum, eget tincidunt tellus ultricies a.
+              </p>
+            </div>
+          </div>
+          <div className={MainStyles.textImageContainer}>
+            <div className={MainStyles.textContainer}>
+              <p className={MainStyles.introText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac justo rhoncus, aliquam magna eu,
+                pulvinar nisi. Suspendisse sit amet sagittis dolor, sed fringilla dui. In sit amet eros et ligula
+                placerat vestibulum. Curabitur lobortis consectetur ipsum, eget tincidunt tellus ultricies a.
+              </p>
+            </div>
+            <div className={MainStyles.imageWrapper}>
+              <LandingImage imageSrc={image} width={500} height={350} />
+            </div>
+          </div>
+        </div>
+        <div className={MainStyles.buttonContainer}>
+          <Link to={"/queries"}>
+            <button className={MainStyles.big_button}>Get started</button>
+          </Link>
+        </div>
+        {showScrollIndicator && (
+          <div className={MainStyles.scrollIndicator} onClick={handleScrollToBottom}>
+            <div className={MainStyles.arrow} />
+          </div>
+        )}
       </div>
-    </span>
+    </div>
   );
 }
+
 export default Main;
