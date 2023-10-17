cf06edc42e8f33ed0633b20ae2ed2afc9966a08d
danibanez <danibanez.info@gmail.com>
2023-10-17 15:39:49 +0200

    binding functionality fixing

--- src/components/BindingsModal/bindingsModal.js
+++ src/components/BindingsModal/bindingsModal.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useRef, useMemo } from "react";
+import React, { useState, useEffect, useCallback, useRef, useMemo } from "react";
 import { CSSTransition } from "react-transition-group";
-import { addSpaceChars } from "../../utils/stringFormatter"
 import BindingModalStyles from "./bindingsModal.module.css";
 import CloseIcon from "@mui/icons-material/Close";
 import DeleteIcon from '@mui/icons-material/RemoveCircleOutline';
@@ -8,23 +7,65 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 // Modal used in binding definitions
 function BindingsModal({ nodes, isBindingsOpen, setBindingsOpen }) {
+    const modalRef = useRef(null);
     const [bindings, setBindings] = useState([]);
     const [operator, setOperator] = useState('+');
-    const [selectedValue1, setSelectedValue1] = useState("");
-    const [selectedValue2, setSelectedValue2] = useState("");
+    const [firstBuilderValue, setFirstBuilderValue] = useState("");
+    const [secondBuilderValue, setSecondBuilderValue] = useState("");
     const [tempBindings, setTempBindings] = useState([]);
     const [bindingName, setBindingName] = useState("");
     const [showInResults, setShowInResults] = useState(false);
     const [showBindingBuilder, setShowBindingBuilder] = useState(bindings.length === 0);
-    const modalRef = useRef(null);
     const operatorList = useMemo(() => (['+', '-', '*', '/']), []);
+
+    const getNumericProperties = useCallback(() => {
+        const nodeNumericValues = (nodes ?? []).flatMap(node => {
+            return Object.entries(node.properties)
+                .filter(([key, property]) => property.type === "number")
+                .map(([key, property]) => ({
+                    label: `${node.label}'s ${key}`,
+                    isFromNode: true,
+                    nodeId: node.id,
+                    propertyUri: property.uri,
+                    value: JSON.stringify({
+                        label: `${node.label}'s ${key}`,
+                        isFromNode: true,
+                        nodeId: node.id,
+                        propertyUri: property.uri
+                    })
+                }));
+        });
+
+        const combinedBindingsValues = bindings.concat(tempBindings).map(binding => ({
+            label: binding.label,
+            isFromNode: false,
+            bindingId: binding.id,
+            value: JSON.stringify({
+                label: binding.label,
+                isFromNode: false,
+                bindingId: binding.id
+            })
+        }));
+
+        return [...nodeNumericValues, ...combinedBindingsValues];
+    }, [nodes, bindings, tempBindings]);
+
+    const setDefaultValuesToFirstOption = useCallback((setSelectedValue) => {
+        const numericProperties = getNumericProperties(nodes, tempBindings);
+        setSelectedValue(numericProperties[0]);
+    }, [nodes, tempBindings, getNumericProperties]);
 
     useEffect(() => {
         if (!isBindingsOpen) setShowBindingBuilder(bindings.length === 0);
-    }, [isBindingsOpen, bindings])
+    }, [isBindingsOpen, bindings]);
+
+    useEffect(() => {
+        if (!firstBuilderValue) setDefaultValuesToFirstOption(setFirstBuilderValue);
+        if (!secondBuilderValue) setDefaultValuesToFirstOption(setSecondBuilderValue);
+    }, [firstBuilderValue, secondBuilderValue, setDefaultValuesToFirstOption]);
 
     const handleClose = () => {
-        setBindings(tempBindings);
+        setTempBindings([]);
         setBindingsOpen(false);
     }
 
@@ -32,24 +73,21 @@ function BindingsModal({ nodes, isBindingsOpen, setBindingsOpen }) {
         setShowBindingBuilder(!showBindingBuilder);
     }
 
-    const MakeItem = (X) => <option key={X.label} value={X.value}>{X.label}</option>;
+    const makeItem = (element) => {
+        return <option key={element.label} value={element.value}>{element.label}</option>;
+    }
 
     const handleOptionChange = (event, element) => {
-        const selectedValue = event.target.value;
-
+        const value = JSON.parse(event.target.options[event.target.selectedIndex].value);
         switch (element) {
             case 1:
-                setSelectedValue1(selectedValue);
+                setFirstBuilderValue(value);
                 break;
             case 2:
-            default:
-                setSelectedValue2(selectedValue);
+                setSecondBuilderValue(value);
                 break;
-        }
-        if (element === 1 && !selectedValue1) {
-            setSelectedValue1(event.target[0]?.value);
-        } else if (element === 2 && !selectedValue2) {
-            setSelectedValue2(event.target[0]?.value);
+            default:
+                break;
         }
     }
 
@@ -71,120 +109,164 @@ function BindingsModal({ nodes, isBindingsOpen, setBindingsOpen }) {
         }
     }
 
+    const findNodeInfo = (value) => {
+        if (!value.isFromNode) {
+            const foundBinding = [...bindings, ...tempBindings].find(binding => binding.id === value.bindingId);
+            return {
+                isFromNode: false,
+                label: foundBinding.label,
+            };
+        } else {
+            const node = nodes.find(n => n.id === value.nodeId);
+            return {
+                isFromNode: true,
+                nodeId: value.nodeId,
+                property: value.propertyUri,
+                label: `${node.label}'s ${value.propertyUri}`,
+            };
+        }
+    }
+
     const addBinding = () => {
         if (!bindingName.trim()) {
             alert('Binding name cannot be empty.');
             return;
         }
-        if (tempBindings.some(b => b.varName === bindingName)) {
+        if (tempBindings.some(b => b.label === bindingName)) {
             alert('Binding with the same name already exists.');
             return;
         }
+
+        const firstValueInfo = findNodeInfo(firstBuilderValue);
+        const secondValueInfo = findNodeInfo(secondBuilderValue);
+
+        if (!firstValueInfo || !secondValueInfo) {
+            alert('Failed to create binding due to an internal error.');
+            return;
+        }
         const newBinding = {
-            varName: bindingName,
-            expression: `${addSpaceChars(selectedValue1)} ${operator} ${addSpaceChars(selectedValue2)}`,
-            showInResults: false,
-            temporary: true
-        };
+            id: Date.now(),
+            label: bindingName,
+            operator: operator,
+            firstValue: firstValueInfo,
+            secondValue: secondValueInfo,
+            showInResults: showInResults
+        }
+
         setTempBindings(prev => [...prev, newBinding]);
     }
 
-
     const handleSubmit = () => {
-        const finalizedBindings = tempBindings.map(binding => {
-            return { ...binding, temporary: false };
-        });
-        setBindings(finalizedBindings);
-        setBindingsOpen(false);
+        setBindings(tempBindings);
+        handleClose();
     }
 
-
-    const handleRemoveVariable = (index) => {
-        const updatedVariables = [...tempBindings];
-        updatedVariables.splice(index, 1);
-        setTempBindings(updatedVariables);
-    }
-
-    const getNumericProperties = (nodes, tempBindings) => {
-        const numericValues = [];
-        [...tempBindings].forEach(binding => {
-            numericValues.push({ label: binding.varName, value: `binding___${binding.varName}` });
-        });
-        nodes?.forEach(node => {
-            Object.keys(node.properties).forEach((key) => {
-                if (node.properties[key].type === "number") {
-                    const optionLabel = `${node.label}'s ${key}`;
-                    numericValues.push({ label: optionLabel, value: `${node.id}___${node.properties[key].uri}` });
-                }
-            });
-        });
-        return numericValues;
-    }
-
-
-    const createOptionElements = (numericProperties) => {
-        if (numericProperties.length === 0) {
-            return <option disabled>No options available</option>;
+    const handleRemoveVariable = (index, source) => {
+        const sourceList = source === 'bindings' ? bindings : tempBindings;
+        const removedValue = JSON.stringify(sourceList[index]);
+        if (removedValue === JSON.stringify(firstBuilderValue)) {
+            console.log("removed");
+            setDefaultValuesToFirstOption(setFirstBuilderValue);
         }
-        return numericProperties.map((item) => (
-            <MakeItem key={item.value} {...item} />
-        ));
+        if (removedValue === JSON.stringify(secondBuilderValue)) {
+            console.log("removed");
+            setDefaultValuesToFirstOption(setSecondBuilderValue);
+        }
+        const updatedVariables = [...sourceList];
+        updatedVariables.splice(index, 1);
+        if (source === 'bindings') {
+            setBindings(updatedVariables);
+        } else {
+            setTempBindings(updatedVariables);
+        }
     }
 
     function bindingBuilder() {
         const numericProperties = getNumericProperties(nodes, tempBindings);
-
-        if (numericProperties.length > 0) {
-            if (!selectedValue1) setSelectedValue1(numericProperties[0].value);
-            if (!selectedValue2) setSelectedValue2(numericProperties[0].value);
-        }
-
-        const optionSet = createOptionElements(numericProperties);
+        const optionSet = numericProperties.map((item) => makeItem(item));
 
         return (
-            <div className={BindingModalStyles.fieldContainer}>
-                <label className={BindingModalStyles.labelVarname}>Variable</label>
-                <span className={BindingModalStyles.inputWrapper}>
-                    <input
+            <div className={BindingModalStyles.bindingBuilder}>
+                <div className={BindingModalStyles.fieldContainer}>
+                    <label className={BindingModalStyles.labelVarname}>Variable</label>
+                    <span className={BindingModalStyles.inputWrapper}>
+                        <input
+                            className={BindingModalStyles.input}
+                            type="text"
+                            value={bindingName}
+                            onChange={e => setBindingName(e.target.value)} />
+                    </span>
+                    <label className={BindingModalStyles.labelEquals}>equals</label>
+                    <select
                         className={BindingModalStyles.input}
-                        type="text"
-                        value={bindingName}
-                        onChange={e => setBindingName(e.target.value)}
-                    />
-                </span>
-                <label className={BindingModalStyles.labelEquals}>equals</label>
-                <select
-                    className={BindingModalStyles.input}
-                    value={selectedValue1}
-                    onChange={(e) => handleOptionChange(e, 1)}
-                >
-                    {optionSet}
-                </select>
-                <button
-                    title={getOperatorTooltip(operator)}
-                    className={BindingModalStyles.operatorButton}
-                    onClick={() => setOperator(getNewOperator(operator))}
-                >
-                    {operator}
-                </button>
-                <select
-                    className={BindingModalStyles.input}
-                    value={selectedValue2}
-                    onChange={(e) => handleOptionChange(e, 2)}
-                >
-                    {optionSet}
-                </select>
-                <label className={BindingModalStyles.labelCheckbox}>Show in results:</label>
+                        value={JSON.stringify(firstBuilderValue)}
+                        onChange={(e) => handleOptionChange(e, 1)}
+                    >
+                        {optionSet}
+                    </select>
+                    <button
+                        title={getOperatorTooltip(operator)}
+                        className={BindingModalStyles.operatorButton}
+                        onClick={() => setOperator(getNewOperator(operator))}
+                    >
+                        {operator}
+                    </button>
+                    <select
+                        className={BindingModalStyles.input}
+                        value={JSON.stringify(secondBuilderValue)}
+                        onChange={(e) => handleOptionChange(e, 2)}
+                    >
+                        {optionSet}
+                    </select>
+                    <label className={BindingModalStyles.labelCheckbox}>Show in results:</label>
+                    <input
+                        className={BindingModalStyles.checkbox}
+                        type="checkbox"
+                        style={{ display: 'inline-block' }}
+                        checked={showInResults}
+                        onChange={e => setShowInResults(e.target.checked)} />
+                    <button className={BindingModalStyles.addButton} onClick={() => addBinding()}>Add binding</button>
+                </div>
+            </div>
+        );
+    }
+
+    const renderBindings = () => {
+        const allBindings = [
+            ...bindings.map(item => ({ ...item, source: 'bindings' })),
+            ...tempBindings.map(item => ({ ...item, source: 'tempBindings' }))
+        ];
+
+        return allBindings.map((binding, index) => (
+            <div
+                key={binding.id}
+                className={BindingModalStyles.bindingRow}
+                style={{ backgroundColor: binding.source === 'tempBindings' ? "#e9e9e9" : "white" }}>
+                <div className={BindingModalStyles.bindingName}>{binding.label}</div>
+                <div className={BindingModalStyles.bindingExpression}>
+                    {binding.firstValue.label} {binding.operator} {binding.secondValue.label}
+                </div>
                 <input
                     className={BindingModalStyles.checkbox}
                     type="checkbox"
                     style={{ display: 'inline-block' }}
-                    checked={showInResults}
-                    onChange={e => setShowInResults(e.target.checked)}
+                    checked={binding.showInResults}
+                    onChange={() => {
+                        const sourceList = binding.source === 'bindings' ? bindings : tempBindings;
+                        const updatedBindings = [...sourceList];
+                        updatedBindings[index].showInResults = !updatedBindings[index].showInResults;
+                        if (binding.source === 'bindings') {
+                            setBindings(updatedBindings);
+                        } else {
+                            setTempBindings(updatedBindings);
+                        }
+                    }}
                 />
-                <button className={BindingModalStyles.addButton} onClick={() => addBinding()}>Add binding</button>
+                <button className={BindingModalStyles.bindingRemove} onClick={() => handleRemoveVariable(index, binding.source)}>
+                    <DeleteIcon />
+                </button>
             </div>
-        );
+        ));
     }
 
     return (
@@ -211,31 +293,8 @@ function BindingsModal({ nodes, isBindingsOpen, setBindingsOpen }) {
                             <h2 title={"Bindings and variables"}>Bindings and variables</h2>
                         </div>
                         <div className={`${BindingModalStyles.modalContent} ${showBindingBuilder ? BindingModalStyles.showBindingBuilder : ""}`}>
-                            <div>
-                                {tempBindings.map((binding, index) => (
-                                    <div key={index} className={BindingModalStyles.bindingRow} style={{ backgroundColor: binding.temporary ? "#e9e9e9" : "white" }}>
-                                        <div className={BindingModalStyles.bindingName}>{binding.varName}</div>
-                                        <div className={BindingModalStyles.bindingExpression}>{binding.expression}</div>
-                                        <input
-                                            className={BindingModalStyles.checkbox}
-                                            type="checkbox"
-                                            style={{ display: 'inline-block' }}
-                                            checked={binding.showInResults}
-                                            onChange={() => {
-                                                const updatedBindings = [...tempBindings];
-                                                updatedBindings[index].showInResults = !updatedBindings[index].showInResults;
-                                                setTempBindings(updatedBindings);
-                                            }}
-                                        />
-                                        <button className={BindingModalStyles.bindingRemove} onClick={() => handleRemoveVariable(index)}>
-                                            <DeleteIcon />
-                                        </button>
-                                    </div>
-                                ))}
-                            </div>
-                            <div className={BindingModalStyles.bindingBuilder}>
-                                {bindingBuilder()}
-                            </div>
+                            {renderBindings()}
+                            {bindingBuilder()}
                         </div>
                         <button className={BindingModalStyles.closeBtn} onClick={handleClose}>
                             <CloseIcon style={{ color: 'white', marginBottom: "-7px" }} />
@@ -244,8 +303,7 @@ function BindingsModal({ nodes, isBindingsOpen, setBindingsOpen }) {
                             <div className={BindingModalStyles.actionsContainer}>
                                 <button
                                     className={BindingModalStyles.setBtn}
-                                    onClick={handleSubmit}
-                                >
+                                    onClick={handleSubmit}>
                                     Set bindings
                                 </button>
                                 <button
@@ -265,7 +323,7 @@ function BindingsModal({ nodes, isBindingsOpen, setBindingsOpen }) {
                 </div>
             </div>
         </CSSTransition>
-    );
+    )
 }
 
 export default BindingsModal;
